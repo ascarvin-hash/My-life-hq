12d0f78b25d6db9a97e1f3977a4dc60e487ff50c
ascarvin-hash <ascarvin@gmail.com>
2026-09-02 14:22:11 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='my-life-hq-v8-5';
+const CACHE='my-life-hq-v8-6';
 const CORE=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
